8c9c0792805c2167c8aea367755ba7d99f6b4294
augustofrade <aquaaticnaotrava@gmail.com>
2025-10-17 23:39:50 -0300

    Add RSS file/url handling from CLI

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -11,26 +11,60 @@ import (
 	rssxmldecoder "github.com/augustofrade/go-rss-aggregator/rss-xml-decoder"
 )
 
+var (
+	file = flag.String("file", "", "Local RSS content file to be parsed")
+	url  = flag.String("url", "", "External RSS content to be fetched and parsed")
+)
+
 func main() {
-	if len(os.Args[1:]) == 0 {
-		log.Fatalln("Expected RSS URL")
-		return
+	flag.Parse()
+
+	var err error
+	var body []byte
+
+	if *file != "" {
+		err = handleLocalFileParse(&body)
+	} else if *url != "" {
+		err = handleUrlParse(&body)
+	} else {
+		panic("--file or --url required")
 	}
-	resp, err := http.Get(os.Args[1])
+
 	if err != nil {
 		panic(err)
+	}
+
+	rssxmldecoder.Decode(&body)
+}
+
+func handleLocalFileParse(body *[]byte) error {
+	fmt.Println("Using file: ", *file)
+
+	var err error
+	*body, err = os.ReadFile(*file)
+
+	return err
+}
+
+func handleUrlParse(body *[]byte) error {
+	fmt.Println("Fetching from URL: ", *file)
+
+	resp, err := http.Get(*url)
+	if err != nil {
+		return err
 	}
 
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	*body, err = io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
-	}
-	contentType := resp.Header.Get("content-type")
-	if !strings.Contains(contentType, "xml") {
-		panic(fmt.Sprintf("Invalid content type: %s", contentType))
+		return err
 	}
 
-	rssxmldecoder.Decode(&body)
+	contentType := resp.Header.Get("content-type")
+	if !strings.Contains(contentType, "xml") {
+		return fmt.Errorf("invalid content type: %s", contentType)
+	}
+
+	return nil
 }
